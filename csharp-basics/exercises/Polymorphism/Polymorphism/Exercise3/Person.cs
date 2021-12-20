931ecb577a9739952286be709750906cf1637f14
Vasily Tchernousov <vtchernousov@gmail.com>
2021-12-20 17:50:00 +0200

    Polymorphism in progress

---
+++ csharp-basics/exercises/Polymorphism/Polymorphism/Exercise3/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    public class Person
+    {
+        private int _id;
+        private static HashSet<int> ids = new HashSet<int>();
+
+        public Person(string firstName, string lastName, string address, int id)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            _id = !ids.Contains(id) ? id : ids.Max() + 1;
+            ids.Add(_id);
+        }
+
+        public Person()
+        {
+            _id = ids.Any()? ids.Max() + 1 : 1;
+            ids.Add(_id);
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Address { get; private set; }
+
+        public void SetNewFirstName(string newName)
+        {
+            FirstName = newName;
+        }
+
+        public void SetNewLastName(string newLastName)
+        {
+            LastName = newLastName;
+        }
+
+        public void SetNewAddress(string newAddress)
+        {
+            Address = newAddress;
+        }
+
+        public virtual void Display() => Console.WriteLine($"Person info: {FirstName} {LastName} {Address}");
+    }
+}
---
+++ csharp-basics/exercises/Polymorphism/Polymorphism/Exercise3/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    public class Student : Person
+    {
+        public Student(double gpa)
+        {
+            Gpa = gpa;
+        }
+
+        public double Gpa { get; }
+
+        public override void Display()
+        {
+            Console.WriteLine($"\tStudent information\n" +
+                              $"First name\t: {FirstName} \n" +
+                              $"Last name\t: {LastName} \n" +
+                              $"Address\t: {Address} \n" +
+                              $"Students GPA\t: {Gpa}");
+        }
+    }
+}
